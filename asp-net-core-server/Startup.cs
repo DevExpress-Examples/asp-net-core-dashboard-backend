123cc43a43dabf36dd1d75a884ca56e4686d7643
Natalia Kazakova (DevExpress) <natalia.kazakova@devexpress.com>
2021-10-06 23:42:45 +0300

    update dasobard controller

--- asp-net-core-server/Startup.cs
+++ asp-net-core-server/Startup.cs
@@ -35,13 +35,15 @@ namespace AspNetCoreDashboardBackend {
                     });
                 })
                 .AddDevExpressControls()
-                .AddControllers()
-                .AddDefaultDashboardController(configurator => {
-                    configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
-                    configurator.SetDataSourceStorage(CreateDataSourceStorage());
-                    configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(Configuration));
-                    configurator.ConfigureDataConnection += Configurator_ConfigureDataConnection;
-                });
+                .AddControllers();
+            services.AddScoped<DashboardConfigurator>((IServiceProvider serviceProvider) => {
+                DashboardConfigurator configurator = new DashboardConfigurator();
+                configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
+                configurator.SetDataSourceStorage(CreateDataSourceStorage());
+                configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(Configuration));
+                configurator.ConfigureDataConnection += Configurator_ConfigureDataConnection;
+                return configurator;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +54,7 @@ namespace AspNetCoreDashboardBackend {
             app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints => {
                 // Maps the dashboard route.
-                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard");
+                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard", "DefaultDashboard");
                 // Requires CORS policies.
                 endpoints.MapControllers().RequireCors("CorsPolicy");
             });
